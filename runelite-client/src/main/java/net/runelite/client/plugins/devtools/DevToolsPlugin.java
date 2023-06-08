01f26704d7c8ffe407f8fe0b0c995eb3233e9cb8
mc <neggie333@gmail.com>
2023-06-08 21:44:59 +1000

    dev tools: added back inventory overlay

--- runelite-client/src/main/java/net/runelite/client/plugins/devtools/DevToolsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/devtools/DevToolsPlugin.java
@@ -127,6 +127,7 @@ public class DevToolsPlugin extends Plugin
 	private DevToolsButton graphicsObjects;
 	private DevToolsButton walls;
 	private DevToolsButton decorations;
+	private DevToolsButton inventory;
 	private DevToolsButton projectiles;
 	private DevToolsButton location;
 	private DevToolsButton chunkBorders;
@@ -169,6 +170,7 @@ public class DevToolsPlugin extends Plugin
 		walls = new DevToolsButton("Walls");
 		decorations = new DevToolsButton("Decorations");
 
+		inventory = new DevToolsButton("Inventory");
 		projectiles = new DevToolsButton("Projectiles");
 
 		location = new DevToolsButton("Location");
