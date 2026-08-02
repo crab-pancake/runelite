635ca70e1d61c1a1457269b7609c7a2e26ed01fb
J G <neggie333@gmail.com>
2026-08-02 12:05:46 +1000

    remove imports

--- runelite-client/src/main/java/net/runelite/client/plugins/clodern/DialogInChat.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/clodern/DialogInChat.java
@@ -1,34 +1,19 @@
 package net.runelite.client.plugins.clodern;
 
-import com.google.inject.Provides;
-import java.awt.Point;
 import java.util.Arrays;
-import java.util.Map;
 import javax.inject.Inject;
-import lombok.Getter;
+
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
-import net.runelite.api.VarClientInt;
 import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.ScriptPostFired;
-import net.runelite.api.events.VarClientIntChanged;
-import net.runelite.api.events.WidgetClosed;
-import net.runelite.api.gameval.InterfaceID;
 import net.runelite.api.gameval.VarbitID;
-import net.runelite.api.widgets.Widget;
 import net.runelite.client.callback.ClientThread;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import static net.runelite.client.plugins.clodern.ClodernConfig.GROUP;
-import net.runelite.client.plugins.clodern.FakeDoor.FakeDoor;
-import net.runelite.client.ui.overlay.OverlayManager;
-import net.runelite.client.ui.overlay.WidgetOverlay;
 
 @Slf4j
 @PluginDescriptor(
